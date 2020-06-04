2c9b69109a18d35f30c742a8ac3c683a897b713c
Johannes Eriksson <johannes.eriksson@hyperisland.se>
2020-06-04 10:55:37 +0200

    add --init-app command

--- manage.py
+++ manage.py
@@ -1,15 +1,28 @@
+import os.path
+
 import click
 
+from app.database import db_file_path
 from app.database import init_db as init_db_fn
 
 
 @click.command()
 @click.option('--init-db', is_flag=True, help='Initialize the database.')
-def execute(init_db):
+@click.option('--init-app', is_flag=True, help='Create and migrate db it does not exist).')  # noqa
+def run(init_db, init_app):
     if init_db:
         init_db_fn()
         print('Initialized the database!')
+        return
+
+    if init_app:
+        if os.path.isfile(db_file_path):
+            print('Database already exists, exiting')
+            return
+
+        init_db_fn()
+        return
 
 
 if __name__ == '__main__':
-    execute()
+    run()
